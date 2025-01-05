759a444946587bab33adf21404770e222b4dd90a
CameronD35 <camster09@ca.rr.com>
2025-01-04 19:19:44 -0800

    Reinstalled THREE.js and imported capsule model.

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
 import * as THREE from 'three';
+import { getViewPosition } from 'three/tsl';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/Addons.js';
 
-
+// SCENE & CAMERA
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color(0x888888)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0xffffff, 1)
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-    cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
-}
-renderer.setAnimationLoop( animate );
-
+// LOADER
 const loader = new GLTFLoader();
 
-loader.load( 'pump_shotgun/scene.gltf', function ( gltf ) {
+const draco = new DRACOLoader();
+draco.setDecoderPath('/examples/jsm/libs/draco');
+loader.setDRACOLoader(draco)
 
-	scene.add( gltf.scene );
 
-}, undefined, function ( error ) {
+// RENDERER
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth * 0.95, window.innerHeight * 0.95);
 
-	console.error( error );
+// ADD ELEMENT
+document.body.appendChild(renderer.domElement);
 
-} );
+// CUBE
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial( {
+	color: 0x8800ff
+});
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+
+// LINE
+const lineMaterial = new THREE.LineBasicMaterial({
+	color: 0x00ff00
+});
+
+const points = [
+	new THREE.Vector3(-2, 0, 0),
+	new THREE.Vector3(0, 2, 0),
+	new THREE.Vector3(2, 0, 0)
+]
+
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(lineGeometry, lineMaterial);
+scene.add(line);
+
+// CAPSULE
+loader.load('capsule/capsule_rev_b.gltf', (gltf) => {
+
+	scene.add(gltf.scene);
+
+	gltf.animations;
+	gltf.scene;
+	gltf.scenes;
+	gltf.cameras;
+	gltf.asset;
+}, 
+(xhr) => {console.log((xhr.loaded/xhr.total * 100) + '% Loaded')},
+(err) => {console.log('error')});
+
+
+// CAMERA ADJUST
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+// ANIMATION
+function animate(){
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	cube.rotation.z += 0.01;
+	document.getElementById('text').textContent = 2;
+
+	line.rotation.x += 0.01;
+	renderer.render(scene, camera);
+}
+
+// START ANIMATION
+renderer.setAnimationLoop(animate);
