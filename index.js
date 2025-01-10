400b9dd8e7d3f42d92de2e755003af1907799691
CameronD35 <camster09@ca.rr.com>
2025-01-09 21:54:58 -0800

    Centered the rotation and position of capsule.

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/Addons.js';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
+const sampleData = [
+	{
+		x: -1.8904000000000000,
+		y: 2.4392000000000000,
+		z: 0.9388000000000000
+	}, 
+	{
+		x: -1.6826,
+		y: -0.8904,
+		z: 2.1708
+	},
+	{
+		x: 2.5608000000000000,
+		y: -0.2560000000000000,
+		z: -1.4146
+	}
+]
+
 // SCENE & CAMERA
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x888888)
@@ -46,8 +64,17 @@ loader.load('capsule/capsule1.gltf', (gltf) => {
 
 	gltf.asset;
 
-	model = gltf.scene
+	model = gltf.scene;
+
+	centerModel(model, 0.01);
+
 	modelLoaded = true;
+
+	let geometry = findType(model, 'Mesh');
+
+	model.prevPositon = model.quaternion;
+
+	addArrowToScene();
 }, 
 (xhr) => {console.log((xhr.loaded/xhr.total * 100) + '% Loaded')},
 (err) => {console.log('error')});
@@ -57,66 +84,117 @@ loader.load('capsule/capsule1.gltf', (gltf) => {
 camera.position.set(0, 3, 5);
 camera.lookAt(0, 0, 0);
 
+// ARROW VARIABLE
+let arrow;
+
 // ANIMATION
 function animate(){
-
-	if (model) {
-		//model.rotation.x += 0.01;
-		getCapsulePosition(model);
-	}
 	renderer.render(scene, camera);
+	
 
-	rotateWithQuaternion(model);
+	if(modelLoaded){
+		//rotateWithQuaternion(model);
+		//console.log(model.prevPositon);
+	}
 }
 
-function getCapsulePosition(capsuleModel) {
-	//let pos = capsuleModel.position;
+function centerModel(obj, precisionAmount){
+	const geometry = findType(obj, 'Mesh');
+	console.log(geometry);
 
-	console.log(capsuleModel.isObject3D);
-	const box = new THREE.Box3().expandByObject(capsuleModel);
+	geometry.center();
+}
 
-	// const posMin = box.min.z;
-	// const posMax = box.max.z;
-	
+// axis (string) - string denoting the axis of which to align the center of the object (x, y, z)
+function alignToAxis(obj, currentAngles, axis){
+	console.log(currentAngles);
+}
 
-
-	// const direction = posMax.sub(posMin).normalize();
-	// console.log(direction)
-
-
-	
-	// ARROW
-
+function addArrowToScene(){
 	const origin = new THREE.Vector3(0, 0, 0);
 
-	const arrow = new THREE.ArrowHelper(new THREE.Vector3(0, 0, 1), origin, 2, 0x00ff00);
+	arrow = new THREE.ArrowHelper(new THREE.Vector3(0, 0, 1), origin, 2, 0x00ff00);
 	scene.add(arrow);
-
-
-
-	//return [pos.x, pos.y, pos.z];
 }
+
+// function getCenterPosition(obj) {
+// 	//let pos = capsuleModel.position;
+
+// 	//console.log(capsuleModel.isObject3D);
+// 	const box = new THREE.Box3().expandByObject(obj);
+
+
+// 	let coords = new THREE.Vector3();
+	
+// 	box.getCenter(coords);
+
+// 	console.log(coords);
+
+// 	return coords;
+
+// 	// const posMin = box.min.z;
+// 	// const posMax = box.max.z;
+	
+
+
+// 	// const direction = posMax.sub(posMin).normalize();
+// 	// console.log(direction)
+
+
+	
+// 	// ARROW
+
+
+
+// 	//return [pos.x, pos.y, pos.z];
+// }
+
+function getCurrentAngle(obj, euler){
+	if (euler){
+		return obj.rotation;
+	}
+
+	return obj.quaternion;
+}
+
 
 function rotateWithQuaternion(object){
 
 	if(modelLoaded){
 
+		const quaternion = new THREE.Quaternion(0, 0.8, 0, 0.6)
 
-
-		const vector = new THREE.Vector4(Math.random(), Math.random(), Math.random(), Math.random())
-
-		const normal = vector.normalize();
-
-		console.log(normal)
-
-		const quaternion = new THREE.Quaternion(normal.x, normal.y, normal.z, normal.w);
 		console.log(object.quaternion);
 
 		object.applyQuaternion(quaternion);
 	}
 }
 
+function calculateNewRotation(prevQuaternion){
+	
+	let matrixA = new THREE.Matrix3()
+	
+}
 
+let test = new THREE.Quaternion(0, 1, 0, 0);
+calculateNewRotation(test);
+console.log(test);
+
+function findType(obj, type){
+	let geometry;
+
+	obj.children.forEach((child) => {
+		if (child.type === type){
+			geometry = child.geometry;
+		}
+	});
+
+	return geometry;
+}
+
+// window.addEventListener('resize', (evt) => {
+// 	let windowDimensions = evt.target.getBoundingClientRect;
+// })
 
 // START ANIMATION
 renderer.setAnimationLoop(animate);
